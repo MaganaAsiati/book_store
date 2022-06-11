f9e8c937027b6a5b6d5bb46c596bba29f0ce3dbe
MaganaAsiati <assia.magana@gmail.com>
2022-06-11 04:20:46 +0200

    get , fetch,remove add

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,9 @@
+import { BASE_URL, APP_ID } from '../../url_conf';
+
 const ADD = 'my-app/books/ADD';
 const REMOVE = 'my-app/books/REMOVE';
+const GET_BOOKS = 'my-app/books/GET_BOOKS';
+
 const defaultState = [
   {
     id: 1,
@@ -16,19 +20,71 @@ const defaultState = [
 export default function bookReducer(state = defaultState, action) {
   switch (action.type) {
     case ADD:
-      return [
-        ...state,
-        action.payload,
-      ];
+      return [...state, action.payload];
     case REMOVE:
       return state.filter((book) => book.id !== action.id);
+    case GET_BOOKS:
+      return action.books;
     default:
       return state;
   }
 }
 export function addBook(book) {
-  return { type: ADD, payload: book };
+  const newBook = {
+    id: book.id,
+    author: book.bookAuthor,
+    title: book.bookTitle,
+  };
+  return { type: ADD, payload: newBook };
 }
-export function removeBook(id) {
+export function getBooks(books) {
+  return {
+    type: GET_BOOKS,
+    books,
+  };
+}
+export function remove(id) {
   return { type: REMOVE, id };
 }
+export function deleteBook(id) {
+  return async (dispatch) => {
+    await fetch(`${BASE_URL}/apps/${APP_ID}/books/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    dispatch(remove(id));
+  };
+}
+
+export function saveBook(book) {
+  return async (dispatch) => {
+    await fetch(`${BASE_URL}/apps/${APP_ID}/books`, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: book.id,
+        title: book.bookTitle,
+        author: book.bookAuthor,
+        category: book.category,
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+    dispatch(addBook(book));
+  };
+}
+
+export const fetchBooks = () => async (dispatch) => {
+  const bookList = await fetch(`${BASE_URL}/apps/${APP_ID}/books`).then(
+    (response) => response.json(),
+  );
+  const books = [];
+  Object.keys(bookList).map((key) => books.push({
+    id: key,
+    title: bookList[key][0].title,
+    author: bookList[key][0].author,
+  }));
+  dispatch(getBooks(books));
+};
