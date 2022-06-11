29cb7195d2c5e4e5097385a34267375e1631b657
MaganaAsiati <assia.magana@gmail.com>
2022-06-11 17:25:34 +0200

    add the gategories of book in the list

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { saveBook } from '../redux/books/books';
+import './Form.css';
 
 export default function Form() {
   const [inputValues, setInputValues] = useState({
@@ -33,20 +34,32 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={submitBook}>
-      <input
-        type="text"
-        placeholder="Title"
-        name="bookTitle"
-        onChange={updateInput}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        name="bookAuthor"
-        onChange={updateInput}
-      />
-      <input type="submit" value="Add Book" />
-    </form>
+    <div className="form">
+      <h3 className="form-head">ADD NEW BOOK</h3>
+      <form onSubmit={submitBook}>
+        <input
+          type="text"
+          placeholder="Title"
+          name="bookTitle"
+          onChange={updateInput}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          name="bookAuthor"
+          onChange={updateInput}
+        />
+        <select name="category" onChange={updateInput}>
+          <option value="" disabled selected>
+            Category
+          </option>
+          <option value="fiction">Fiction</option>
+          <option value="action">Action</option>
+          <option value="fantaisy">Fantaisy</option>
+          <option value="drama">Drama</option>
+        </select>
+        <input className="form-but" type="submit" value="ADD BOOK" />
+      </form>
+    </div>
   );
 }
